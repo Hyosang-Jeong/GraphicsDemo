635c641c8f3aaae5fd9b1d0142264a60f33d266a
JaewooChoi <wod6983@naver.com>
2022-05-23 13:59:36 +0900

    update Shadow and shader

--- class_project/tests/Shadow.cpp
+++ class_project/tests/Shadow.cpp
@@ -23,13 +23,26 @@ Shadow_test::~Shadow_test()
 void Shadow_test::init()
 {
     mesh = CreateCube(30, 30);
-    plane = CreatePlane(30, 30);
     sphere = CreateSphere(30, 30);
+    plane = CreatePlane(30, 30);
 
     mesh.init("depth", { 0, 0, 0 }, { 0.5,0.5,0.5 });
-    plane.init("depth", { 0,-0.5,0 }, { 100,100,100 }, { -HALF_PI,0,0 });
     sphere.init("depth", { 3.f, 0, 0 }, { 0.5,0.5,0.5 });
+    plane.init("depth", { 0,-0.5,0 }, { 100,100,100 }, { -HALF_PI,0,0 });
+    mesh.setup_shdrpgm("debugDepthQuad");
+    glUseProgram(mesh.renderProg.GetHandle());
+    glUniform1f(glGetUniformLocation(mesh.renderProg.GetHandle(), "near_plane"), near_plane);
+    glUniform1f(glGetUniformLocation(mesh.renderProg.GetHandle(), "far_plane"), far_plane);
 
+    sphere.setup_shdrpgm("debugDepthQuad");
+    glUseProgram(sphere.renderProg.GetHandle());
+    glUniform1f(glGetUniformLocation(sphere.renderProg.GetHandle(), "near_plane"), near_plane);
+    glUniform1f(glGetUniformLocation(sphere.renderProg.GetHandle(), "far_plane"), far_plane);
+
+    plane.setup_shdrpgm("debugDepthQuad");
+    glUseProgram(plane.renderProg.GetHandle());
+    glUniform1f(glGetUniformLocation(plane.renderProg.GetHandle(), "near_plane"), near_plane);
+    glUniform1f(glGetUniformLocation(mesh.renderProg.GetHandle(), "far_plane"), far_plane);
     camera = { {0,0,3} };
     light = { -2.0f, 4.0f, 0.0f };
     FOV = 45.f;
@@ -45,15 +58,21 @@ void Shadow_test::init()
 void Shadow_test::Update(float deltaTime)
 {
     camera.Update(deltaTime);
+    light.x = sin(glfwGetTime()) * 3.0f;
+    light.z = cos(glfwGetTime()) * 2.0f;
+    light.y = 5.0 + cos(glfwGetTime()) * 1.0f;
 }
 
 void Shadow_test::Draw()
 {
     glClearColor(0.5, 0.5, 0.5, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     Depth_Draw();
     Scene_Draw();
-     
+
+
+
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
@@ -69,11 +88,22 @@ void Shadow_test::DepthMap_Setup()
 {
     glGenTextures(1, &depthMap); 
     glBindTexture(GL_TEXTURE_2D, depthMap);
-
+    //GLenum depth_component = GL_DEPTH_COMPONENT32;
+    //if (depthBitSize == DepthComponentSize::Bit16)
+    //    depth_component = GL_DEPTH_COMPONENT16;
+    //else if (depthBitSize == DepthComponentSize::Bit24)
+    //    depth_component = GL_DEPTH_COMPONENT24;
+    //glTexStorage2D(GL_TEXTURE_2D, 1, depth_component, SHADOW_WIDTH, SHADOW_HEIGHT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER); 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    float borderColor[] = { 1.0f,1.0f,1.0f,1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    //glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR,border);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LESS);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
 
@@ -88,11 +118,26 @@ void Shadow_test::DepthMap_Setup()
 void Shadow_test::Depth_Draw()
 {
 
-    glm::mat4 lightProjection = glm::perspective(glm::radians(FOV), (float)GLHelper::width / (float)GLHelper::height, 0.1f, 100.0f);
+    glm::mat4 lightProjection = glm::perspective(glm::radians(FOV), (float)GLHelper::width / (float)GLHelper::height, near_plane, far_plane);
     glm::mat4 lightView = glm::lookAt(light,
                                                                     glm::vec3(0.0f, 0.0f, 0.0f),
                                                                  glm::vec3(0.0f, 1.0f, 0.0f));
     glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+
+    //simpleDepthShader.use();
+    //simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+    //glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+    //glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    //glClear(GL_DEPTH_BUFFER_BIT);
+    //glActiveTexture(GL_TEXTURE0);
+    //glBindTexture(GL_TEXTURE_2D, woodTexture);
+    //renderScene(simpleDepthShader);
+    //glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    //// reset viewport
+    //glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
     glClear(GL_DEPTH_BUFFER_BIT);
@@ -154,6 +199,7 @@ void Shadow_test::Scene_Draw()
     glUniform1i(modeLoc, 0);
     glUniformMatrix4fv(shadowLoc, 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
     sphere.draw({ 1,0,0 }, camera.GetViewMatrix(), projection, light, camera.GetEye());
+
     plane.renderProg.Use();
      modeLoc = glGetUniformLocation(plane.renderProg.GetHandle(), "mode");
      shadowLoc = glGetUniformLocation(plane.renderProg.GetHandle(), "shadowMatrix");
--- shaders/depth.frag
+++ shaders/depth.frag
@@ -22,29 +22,33 @@ vec3 viewDir = normalize(viewPos - FragPos);
 
 float ShadowCalculation(vec4 fragPosLightSpace) 
 {
-        if(FragPos.x <-1|| FragPos.x >1)
-        {
-            float shadow = 0;
-            return shadow;
-        }
-        if(FragPos.y <-1|| FragPos.y >1)
-        {
-            float shadow = 0;
-            return shadow;
-        }
-       if(FragPos.z <-1|| FragPos.z >1)
-        {
-            float shadow = 0;
-            return shadow;
-        }
+
+
         vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w; 
 
         projCoords = projCoords * 0.5 + 0.5;     
 
         float closestDepth = texture(shadowMap, projCoords.xy).r; 
         float currentDepth = projCoords.z; // check whether current frag pos is in shadow 
+        float bias = max(0.05 * (1.0 - dot(norm, lightDir)), 0.005);
         float shadow = currentDepth > closestDepth ? 1.0 : 0.0; 
 
+        vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+        for(int x = -1; x <= 1; ++x)
+        {
+            for(int y = -1; y <= 1; ++y)
+            {
+                float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+                shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
+            }    
+        }
+        shadow /= 9.0;
+
+        if(projCoords.z >1.0)
+        {
+            shadow = 0;
+
+        }
         return shadow;
 }
 
@@ -54,9 +58,11 @@ void main()
     if(mode == 0) //basic scene
     {
             float shadow =ShadowCalculation(shadowpos);
+
             outColor =  color * vec4(lightColor,1.0);
-			outColor= vec4(diff *outColor);
+			         outColor= vec4(diff *outColor);
             outColor = vec4(vec3(outColor.xyz)*(1-shadow),1);
+
 
     }
     else
